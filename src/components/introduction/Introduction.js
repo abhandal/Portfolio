0711f7bf88b874c73930d0f7572b1939557a9df6
Aman  Bhandal <aman_bhandal@live.ca>
2019-09-08 12:57:01 -0400

    Added introduction page

---
+++ src/components/introduction/Introduction.js
@@ -0,0 +1,29 @@
+import React from 'react'
+import './Introduction.css'
+
+export function Introduction() 
+{
+    return (
+        <div class="Introduction-container">
+            <h3 class="Introduction-header">
+                Hi, my name is
+            </h3>
+            <h1 class="Introduction-name">Aman Bhandal.</h1>
+            <h1 class="Introduction-tagline">I build things.</h1>
+            <div class="Introduction-paragraph-container">
+                <p class="Introduction-paragraph">
+                I am a software engineer from Montreal, QC specializing in 
+                full-stack development with a knack for design.
+                </p>
+            </div>
+
+            <div class="Introduction-button-container">
+                <a class="Introduction-button-anchor" href="mailto: aman.bhandal@outlook.com">
+                    <button class="Introduction-button">
+                        Contact Me
+                    </button>
+                </a>
+            </div>
+        </div>
+    );
+}
